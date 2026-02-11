d4bf3dc6701b8a807136f9ded3634ae91d1321ad
NoRazoom <kakovtoktoto@yandex.ru>
2026-02-11 20:52:46 +0300

    first review bad-request+

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-test-key-12345')
 
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 kittygram-friends.online').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
